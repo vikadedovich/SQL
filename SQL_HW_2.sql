e609b930437511524171d72cc24fb75598478e2e
vikadedovich <vikikeller465@gmail.com>
2023-11-08 21:44:23 +0100

    update

--- SQL_HW_2.sql
+++ SQL_HW_2.sql
@@ -3,7 +3,7 @@
 select * from students;
 
 --2. Вывести всех студентов в таблице
-select name from students;
+select * from students;
 
 --3. Вывести только Id пользователей
 select id from students;
@@ -21,52 +21,52 @@ select name, email from students;
 select id, name, email, created_on from students;
 
 --8. Вывести пользователей где password 12333
-select * from students where password like '12333';
+select * from students where password = '12333';
 
 --9. Вывести пользователей которые были созданы 2021-03-26 00:00:00
 select * from students where created_on = '2021-03-26 00:00:00';
 
 --10. Вывести пользователей где в имени есть слово Анна
-select name from students where name like '%Anna%';
+select * from students where name like '%Anna%';
 
 --11. Вывести пользователей где в имени в конце есть 8
-select name from students where name like '%8';
+select * from students where name like '%8';
 
 --12. Вывести пользователей где в имени в есть буква а
-select name from students where name like '%a%';
+select * from students where name like '%a%';
 
 --13. Вывести пользователей которые были созданы 2021-07-12 00:00:00
 select * from students where created_on = '2021-07-12 00:00:00';
 
 --14. Вывести пользователей которые были созданы 2021-07-12 00:00:00 и имеют пароль 1m313
-select * from students where created_on = '2021-07-12 00:00:00' and password like '1m313';
+select * from students where created_on = '2021-07-12 00:00:00' and password = '1m313';
 
 --15. Вывести пользователей которые были созданы 2021-07-12 00:00:00 и у которых в имени есть слово Andrey
 select * from students where created_on = '2021-07-12 00:00:00' and name like '%Andrey%';
 
 --16. Вывести пользователей которые были созданы 2021-07-12 00:00:00 и у которых в имени есть цифра 8
-select * from students where created_on = '2021-07-12 00:00:00' and name like '%8';
+select * from students where created_on = '2021-07-12 00:00:00' and name like '%8%';
 
 --17. Вывести пользователя у которых id равен 110
-select * from students s where id = '110';
+select * from students s where id = 110;
 
 --18. Вывести пользователя у которых id равен 153
-select * from students s where id = '153';
+select * from students s where id = 153;
 
 --19. Вывести пользователя у которых id больше 140
-select * from students s where id > '140';
+select * from students s where id > 140;
 
 --20. Вывести пользователя у которых id меньше 130
-select * from students s where id < '130';
+select * from students s where id < 130;
 
 --21. Вывести пользователя у которых id меньше 127 или больше 188
 select * from students s where id not between 127 and 188;
 
 --22. Вывести пользователя у которых id меньше либо равно 137
-select * from students s where id <= '137';
+select * from students s where id <= 137;
 
 --23. Вывести пользователя у которых id больше либо равно 137
-select * from students s where id >= '137';
+select * from students s where id >= 137;
 
 --24. Вывести пользователя у которых id больше 180 но меньше 190
 select * from students s where id > 180 and id < 190;
@@ -93,4 +93,4 @@ select count(name) from students;
 select id, name, created_on from students order by created_on asc;
 
 --32. Вывести id пользователя, имя, дату создания пользователя. Отсортировать по порядку убывания даты добавления пользоватлеля.
-select id, name, created_on from students s order by created_on desc;
+select id, name, created_on from students order by created_on desc;
